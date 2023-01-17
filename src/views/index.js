65a18938910afccac210a540678e8641ea6d2932
Iury H. Flores <iury.flores@gmail.com>
2023-01-17 11:18:07 -0300

    Fix params

--- src/views/index.js
+++ src/views/index.js
@@ -3,4 +3,6 @@ export * from "./SendComplaint.js";
 export * from "./TrackComplaint.js";
 export * from "./Relationship";
 export * from "./MoralHarassment";
+export * from "./AddForm.js";
 export * from "./HomePage.js";
+
