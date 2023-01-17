b955792b1f3f03ec32b0138a5a8ce891eaceb34f
Iury H. Flores <iury.flores@gmail.com>
2023-01-17 08:13:32 -0300

    Creating form for complaints

--- src/App.js
+++ src/App.js
@@ -10,18 +10,79 @@ import {
   Relationship,
   MoralHarassment,
 } from "./views/index.js";
+import { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/complaints" element={<Complaints />} />
-        <Route path="/relationship" element={<Relationship />} />
-        <Route path="/moral_harassment" element={<MoralHarassment />} />
-        <Route path="/send-complaint" element={<SendComplaint />} />
-        <Route path="/track-complaint" element={<TrackComplaint />} />
+        <Route
+          path="/"
+          element={
+            <HomePage
+              message={message}
+              setMessage={setMessage}
+              loading={loading}
+              setLoading={setLoading}
+            />
+          }
+        />
+        <Route
+          path="/complaints"
+          element={
+            <Complaints
+              message={message}
+              setMessage={setMessage}
+              loading={loading}
+              setLoading={setLoading}
+            />
+          }
+        />
+        <Route
+          path="/relationship"
+          element={<Relationship />}
+          message={message}
+          setMessage={setMessage}
+          loading={loading}
+          setLoading={setLoading}
+        />
+        <Route
+          path="/moral_harassment"
+          element={
+            <MoralHarassment
+              message={message}
+              setMessage={setMessage}
+              loading={loading}
+              setLoading={setLoading}
+            />
+          }
+        />
+        <Route
+          path="/send-complaint"
+          element={
+            <SendComplaint
+              message={message}
+              setMessage={setMessage}
+              loading={loading}
+              setLoading={setLoading}
+            />
+          }
+        />
+        <Route
+          path="/track-complaint"
+          element={
+            <TrackComplaint
+              message={message}
+              setMessage={setMessage}
+              loading={loading}
+              setLoading={setLoading}
+            />
+          }
+        />
       </Routes>
       <Footer />
     </>
