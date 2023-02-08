e0473aee5d147fe98df08605b7975a30e4590592
iuryflores <iury.flores@gmail.com>
2023-02-08 16:37:40 -0300

    Feat:Rendering details from api

--- src/views/TrackingComplaint.js
+++ src/views/TrackingComplaint.js
@@ -24,14 +24,44 @@ export const TrackingComplaint = ({ loading, setLoading }) => {
 
   return !loading ? (
     <div>
-      {dataAPI.map((complaint) => {
+      {dataAPI.map((complaint, index) => {
+        let category = complaint.complaint_ID.category;
+
+        if (category === "moral_harassment") {
+          category = "Assédio Moral";
+        }
+
         return (
-          <>
-            <p>Data: {complaint.complaint_ID.createdAt}</p>
-            <p>Protocolo n: {complaint.protocolo_id}</p>
-            <p>Categoria: {complaint.complaint_ID.category}</p>
-            <p>Departamento: {complaint.complaint_ID.sector}</p>
-          </>
+          <div className="container bg-light d-flex flex-column" key={index}>
+            <h5>Dados do reclamante</h5>
+            {complaint.complaint_ID.name ? (
+              <>
+                {" "}
+                <span>Nome: {complaint.complaint_ID.name}</span>
+                <span>E-mail: {complaint.complaint_ID.email}</span>
+                <span>Telefone: {complaint.complaint_ID.telephone}</span>
+                <span>Cargo: {complaint.complaint_ID.role}</span>
+              </>
+            ) : (
+              <>Denúncia Anônima</>
+            )}
+
+            <hr />
+            <h5>Dados da reclamação</h5>
+            <span>Data: {complaint.complaint_ID.createdAt}</span>
+            <span>Protocolo n: {complaint.protocolo_id}</span>
+            <span>
+              Denúncia anterior n:
+              {complaint.complaint_ID.previousComplaint}
+            </span>
+            <span>Categoria: {category}</span>
+            <span>Departamento: {complaint.complaint_ID.sector}</span>
+            <span>Envolvidos: {complaint.complaint_ID.involved}</span>
+            <span>Quem presenciou: {complaint.complaint_ID.witness}</span>
+            <span>Conversou com: {complaint.complaint_ID.talkedAbout}</span>
+            <span>Detalhes: {complaint.complaint_ID.details}</span>
+            <span>Situação: {complaint.complaint_ID.status}</span>
+          </div>
         );
       })}
     </div>
