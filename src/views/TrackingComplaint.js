9f4dc5c3a8ab76f875a84058f3b695edf62b1177
iuryflores <iury.flores@gmail.com>
2023-02-08 16:16:55 -0300

    Fix: returning data from api

--- src/views/TrackingComplaint.js
+++ src/views/TrackingComplaint.js
@@ -1,29 +1,41 @@
-import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import api from "../utils/api.utils";
 import { useState } from "react";
 
-export const TrackingComplaint = () => {
-  const navigate = useNavigate();
+export const TrackingComplaint = ({ loading, setLoading }) => {
   const { protocolo_id, pass_protocolo } = useParams();
 
-  const [dataAPI, setDataAPI] = useState("");
+  const [dataAPI, setDataAPI] = useState([]);
 
-  console.log(protocolo_id, pass_protocolo);
-  const getData = async () => {
-    try {
-      const data = await api.trackComplaint(protocolo_id, pass_protocolo);
-      setDataAPI(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  getData();
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await api.trackComplaint(protocolo_id, pass_protocolo);
+        setDataAPI(data);
+        setLoading(false);
+        console.log(data);
+      } catch (error) {
+        console.log(error, "Error to get data");
+      }
+    };
+    getData();
+  }, [setLoading, protocolo_id, pass_protocolo]);
 
-  return (
+  return !loading ? (
     <div>
-      {dataAPI}
-      TrackingComplaint
+      {dataAPI.map((complaint) => {
+        return (
+          <>
+            <p>Data: {complaint.complaint_ID.createdAt}</p>
+            <p>Protocolo n: {complaint.protocolo_id}</p>
+            <p>Categoria: {complaint.complaint_ID.category}</p>
+            <p>Departamento: {complaint.complaint_ID.sector}</p>
+          </>
+        );
+      })}
     </div>
+  ) : (
+    <>Loading...</>
   );
 };
