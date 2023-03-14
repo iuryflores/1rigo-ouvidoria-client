9c59e87f218442d7d7c8e37aecd115186f4ce002
iury.admin <iury.flores@gmail.com>
2023-03-14 17:28:32 -0300

    Fix: manifestacao page

--- src/views/ouvidoria/Manifestacao.js
+++ src/views/ouvidoria/Manifestacao.js
@@ -32,18 +32,18 @@ export const Manifestacao = ({ loading, setLoading, message, setMessage }) => {
   const [messageUser, setMessageUser] = useState("");
 
   useEffect(() => {
-    const getOuvidorias = async () => {
+    const getOuvidoria = async () => {
       try {
-        const data = await api.getOuvidorias();
+        const data = await api.getOuvidoria(id);
         setOuvidorias(data);
         setTimeout(() => {
           setLoading(false);
         }, 2000);
       } catch (error) {
-        console.log(error, "Error to get ouvidorias");
+        console.log(error, "Error to get ouvidoria");
       }
     };
-    getOuvidorias();
+    getOuvidoria();
 
     const getAuditById = async () => {
       try {
